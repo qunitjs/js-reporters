ba6647ed99b36df2d641454e325b294a65d8e344
Franziska Carstens <franziska.carstens@wwu.de>
2015-07-11 14:11:55 +0200

    All: Import QUnitAdapter and TapReporter with build setup and tests
    Closes #13

---
+++ test/qunit/tests.js
@@ -0,0 +1,17 @@
+QUnit.module("group a");
+QUnit.test("foo", function (assert) {
+    assert.equal(5, "5");
+    assert.equal(1 / 0, Infinity);
+});
+QUnit.test("bar goes wrong", function (assert) {
+    assert.deepEqual(5, "5");
+});
+
+QUnit.module("group b");
+QUnit.test("baz", function (assert) {
+    assert.ok(true);
+});
+
+QUnit.skip("skipped test", function (assert) {
+    assert.ok(true)
+});
