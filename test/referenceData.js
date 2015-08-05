939a8d37e3e45a75086d42ef4f4e90f40d425828
Franziska Carstens <franziska.carstens@wwu.de>
2015-08-05 13:21:55 +0200

    remove skipped attribute

--- test/referenceData.js
+++ test/referenceData.js
@@ -3,11 +3,11 @@ var Test = JsReporters.Test;
 var Suite = JsReporters.Suite;
 
 
-var foo = new Test("foo", "group a", "passed", 1, [], false);
-var bar = new Test("bar goes wrong", "group a", "failed", 1, [{}], false);
+var foo = new Test("foo", "group a", "passed", 1, []);
+var bar = new Test("bar goes wrong", "group a", "failed", 1, [{}]);
 var baz = new Test("baz", "group b", "passed", 1, [], false);
-var skipped = new Test("skipped test", "group b", "skipped", 1, [], true);
-var subtest = new Test("subtest", "group with subgroup subgroup", "passed", 1, [], false);
+var skipped = new Test("skipped test", "group b", "skipped", 1, []);
+var subtest = new Test("subtest", "group with subgroup subgroup", "passed", 1, []);
 
 var groupA = new Suite("group a", [], [foo, bar]);
 var groupB = new Suite("group b", [], [baz, skipped]);
