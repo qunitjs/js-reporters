cfa1923f8b4c86691eb4ee7b339d0191a4bd1bd7
Franziska Carstens <franziska.carstens@wwu.de>
2015-08-07 16:52:48 +0200

    Test: add QUnit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 /* eslint camelcase:0 */
 var Jasmine = require("jasmine");
+QUnit = require("qunitjs");
 var JsReporters = require("../dist/js-reporters.js");
 var referenceData = require("./referenceData.js");
+
 
 var jasmine = new Jasmine();
 jasmine.loadConfig({
@@ -11,11 +13,19 @@ jasmine.loadConfig({
     ]
 });
 
-var runner = new JsReporters.JasmineAdapter(jasmine.env);
+var jasmineRunner = new JsReporters.JasmineAdapter(jasmine.env);
 jasmine.addReporter({}); // Suppress the default reporter
-var testReporter = new JsReporters.TestReporter(runner, referenceData.Jasmine);
+var jasmineTestReporter = new JsReporters.TestReporter(jasmineRunner, referenceData.Jasmine);
 
 jasmine.execute();
-if (!testReporter.ok) {
+if (!jasmineTestReporter.ok) {
     throw new Error("Tests for JasmineAdapter failed!");
 }
+
+QUnit.config.autorun = false;
+
+require("./qunit/tests.js");
+var qunitRunner = new JsReporters.QUnitAdapter();
+var qunitTestReporter = new JsReporters.TestReporter(qunitRunner, referenceData.QUnit);
+
+QUnit.load();
