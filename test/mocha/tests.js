79587641282eb03176d5d9bb1ef6386c1ea0caea
Franziska Carstens <franziska.carstens@wwu.de>
2015-08-20 18:17:13 +0200

    Adapter: add mocha adapter

---
+++ test/mocha/tests.js
@@ -0,0 +1,27 @@
+describe("group a", function () {
+    it("foo", function () {
+        assert(5 == 5);
+        assert(1 / 0 == Infinity);
+    });
+
+    it("bar goes wrong", function () {
+        assert(5 === "5");
+    });
+});
+
+describe("group b", function () {
+    it("baz", function () {
+        assert(true);
+    });
+    xit("skipped test", function () {
+        assert(true);
+    });
+
+});
+describe("group with subgroup", function () {
+    describe("subgroup", function () {
+        it("subtest", function () {
+            assert(true);
+        });
+    });
+});
