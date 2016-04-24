447c30062e4cb6d7e21f1ea255a16e27712f4dce
Florentin <florentins@gmail.com>
2016-04-24 14:38:42 +0300

    Bundling: Switch to a rollup config file, use babelrc file
    Fixes #42
    Closes #43

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var babel = require('rollup-plugin-babel');
+var pkg = require('./package.json');
+
+var licenseHeader = fs.readFileSync('license-header', {encoding: 'utf8'})
+      .replace('@VERSION', pkg.version)
+      .replace('@DATE', (new Date()).toISOString().replace(/:\d+\.\d+Z$/, "Z"));
+
+module.exports = {
+  entry: 'lib/index.js',
+  dest: 'dist/js-reporters.js',
+  format: 'umd',
+  moduleName: 'JsReporters',
+  banner: licenseHeader,
+  plugins: [
+    babel()
+  ]
+};
