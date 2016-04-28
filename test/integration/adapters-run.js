fe375739c966e6f2f880722055054838a16f3c5b
Florentin <florentins@gmail.com>
2016-04-28 20:43:57 +0300

    Testing: Use absolute paths for Mocha and QUnit.

--- test/integration/adapters-run.js
+++ test/integration/adapters-run.js
@@ -2,6 +2,21 @@ var Jasmine = require('jasmine')
 var QUnit = require('qunitjs')
 var Mocha = require('mocha')
 var JsReporters = require('../../dist/js-reporters.js')
+var path = require('path')
+
+/**
+ * @type {String}
+ * @const
+ * @default
+ */
+var testDir = path.join(__dirname, '..')
+
+/**
+ * @type {String}
+ * @const
+ * @default
+ */
+var testFile = 'tests.js'
 
 /**
  * Exports a function for each adapter that will run
@@ -12,11 +27,10 @@ module.exports = {
     var jasmine = new Jasmine()
     var jasmineRunner
 
+
     jasmine.loadConfig({
       spec_dir: 'test/jasmine',
-      spec_files: [
-        'tests.js'
-      ]
+      spec_files: [testFile]
     })
 
     jasmineRunner = new JsReporters.JasmineAdapter(jasmine.env)
@@ -34,7 +48,7 @@ module.exports = {
 
     QUnit.config.autorun = false
 
-    require('../qunit/tests.js')
+    require(path.join(testDir, 'qunit', testFile));
 
     QUnit.load()
   },
@@ -43,7 +57,7 @@ module.exports = {
     var mocha = new Mocha()
     var mochaRunner
 
-    mocha.addFile('test/mocha/tests.js')
+    mocha.addFile(path.join(testDir, 'mocha', testFile))
 
     mochaRunner = new JsReporters.MochaAdapter(mocha)
 
