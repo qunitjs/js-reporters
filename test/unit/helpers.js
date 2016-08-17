9ba5d152401233607541f1befb76fbad1d40c930
Florentin Simion <florentins@gmail.com>
2016-08-17 21:31:44 +0100

    Testing: finish unit tests for the autoRegister function

--- test/unit/helpers.js
+++ test/unit/helpers.js
@@ -8,16 +8,64 @@ var expect = chai.expect
 chai.use(require('sinon-chai'))
 
 describe('Helpers', function () {
+  var dummyFunc = function () {}
+
   describe('autoregister', function () {
     it('should register the QUnitAdapter', function () {
-      GLOBAL.QUnit = {}
-      var spy = sinon.stub(JsReporters, 'QUnitAdapter')
+      GLOBAL.QUnit = {
+        begin: sinon.stub(),
+        testStart: dummyFunc,
+        log: dummyFunc,
+        testDone: dummyFunc,
+        done: dummyFunc
+      }
+
+      JsReporters.autoRegister()
+
+      expect(GLOBAL.QUnit.begin).to.have.been.calledOnce
+
+      delete GLOBAL.QUnit
+    })
+
+    it('should register the MochaAdapter', function () {
+      GLOBAL.QUnit = undefined
+      GLOBAL.mocha = {
+        reporter: sinon.stub()
+      }
+
+      JsReporters.autoRegister()
+
+      expect(GLOBAL.mocha.reporter).to.have.been.calledOnce
+
+      delete GLOBAL.Jasmine
+    })
+
+    it('should register the JasmineAdapter', function () {
+      var spy = sinon.stub()
+      GLOBAL.QUnit = undefined
+      GLOBAL.mocha = undefined
+      GLOBAL.jasmine = {
+        getEnv: function () {
+          return {
+            addReporter: spy
+          }
+        }
+      }
 
       JsReporters.autoRegister()
 
       expect(spy).to.have.been.calledOnce
 
-      delete GLOBAL.QUnit
+      delete GLOBAL.Jasmine
+    })
+
+    it('should throw an error if no testing framework was found', function () {
+      GLOBAL.QUnit = undefined
+      GLOBAL.mocha = undefined
+      GLOBAL.jasmine = undefined
+
+      expect(JsReporters.autoRegister).to.throw(Error)
     })
   })
 })
+
