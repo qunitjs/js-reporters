3d97567088e873480026ec0a91487159b5a3ac73
Florentin Simion <florentins@gmail.com>
2016-09-02 12:52:54 +0300

    Testing: update failing versions

--- test/versions/failing-versions.js
+++ test/versions/failing-versions.js
@@ -1,9 +1,12 @@
 /**
  * Here is a brief list with the known issues.
  *
- * Jasmine 2.3.0: the Jasmine adapter is working, something happens during
- * testing with our testing framework Mocha, which is receiving SIGINT and aborts its runner,
- * it may be a problem with the testing not with the adapter itself.
+ * Jasmine 2.3.0: there is a bug that makes Jasmine to exit and kill the
+ * process. This is solved in the 2.3.1 version. @see
+ * https://github.com/jasmine/jasmine-npm/blob/master/release_notes/2.3.1.md
+ *
+ * Jasmine 2.5.0: it is the same bug as in the 2.3.0 version. @see
+ * https://github.com/jasmine/jasmine-npm/issues/88
  *
  * Mocha 2.5.0: is not working because of a missing dependency, this is Mocha's
  * fault @see
@@ -13,7 +16,7 @@
 module.exports = {
   'qunitjs': ['1.9.0', '1.10.0', '1.11.0', '1.12.0-pre', '1.12.0', '1.13.0',
     '1.14.0', '1.15.0', '1.16.0', '1.17.0', '1.17.1', '1.18.0', '1.19.0'],
-  'jasmine': ['2.3.0'],
+  'jasmine': ['2.3.0', '2.5.0'],
   'mocha': ['0.3.0', '0.3.1', '0.3.2', '0.3.3', '0.3.4', '0.3.6', '0.4.0',
     '0.5.0', '0.6.0', '0.7.0', '0.7.1', '0.8.0', '0.9.0', '0.10.0', '0.10.1',
     '0.10.2', '0.11.0', '0.12.0', '0.12.1', '0.13.0', '0.14.0', '0.14.1',
