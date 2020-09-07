ee2adf3c0327b8e48eef8510d83e1977727b550a
Timo Tijhof <krinklemail@gmail.com>
2020-09-07 21:19:42 +0100

    Test: Update failing-versions.js
    Most older QUnit versions are passing now, yay.

--- test/versions/failing-versions.js
+++ test/versions/failing-versions.js
@@ -1,29 +1,92 @@
 /**
- * Here is a brief list with the known issues.
- *
- * Jasmine 2.3.0: there is a bug that makes Jasmine to exit and kill the
- * process. This is solved in the 2.3.1 version. @see
- * https://github.com/jasmine/jasmine-npm/blob/master/release_notes/2.3.1.md
- *
- * Jasmine 2.5.0: it is the same bug as in the 2.3.0 version. @see
- * https://github.com/jasmine/jasmine-npm/issues/88
- *
- * Mocha 2.5.0: is not working because of a missing dependency, this is Mocha's
- * fault @see
- * https://github.com/mochajs/mocha/blob/master/CHANGELOG.md#251--2016-05-23.
+ * Known issues.
  */
 
 module.exports = {
-  qunitjs: ['1.9.0', '1.10.0', '1.11.0', '1.12.0-pre', '1.12.0', '1.13.0',
-    '1.14.0', '1.15.0', '1.16.0', '1.17.0', '1.17.1', '1.18.0', '1.19.0'],
-  jasmine: ['2.3.0', '2.5.0'],
-  mocha: ['0.3.0', '0.3.1', '0.3.2', '0.3.3', '0.3.4', '0.3.6', '0.4.0',
-    '0.5.0', '0.6.0', '0.7.0', '0.7.1', '0.8.0', '0.9.0', '0.10.0', '0.10.1',
-    '0.10.2', '0.11.0', '0.12.0', '0.12.1', '0.13.0', '0.14.0', '0.14.1',
-    '1.0.0', '1.0.1', '1.0.2', '1.0.3', '1.1.0', '1.2.0', '1.2.1', '1.2.2',
-    '1.3.0', '1.3.1', '1.3.2', '1.4.0', '1.4.1', '1.4.2', '1.4.3', '1.5.0',
-    '1.6.0', '1.7.0', '1.7.1', '1.7.2', '1.7.3', '1.7.4', '1.8.0', '1.8.1',
-    '1.8.2', '1.9.0', '1.10.0', '1.11.0', '1.12.0', '1.12.1', '1.13.0',
-    '1.14.0', '1.15.0', '1.15.1', '1.16.0', '1.16.1', '1.16.2', '1.17.0',
-    '1.17.1', '2.1.0', '2.5.0']
+  // The package name 'qunitjs' is deprecated in favour of 'qunit'.
+  // It provides qunitjs@1.9.0 until qunitjs@2.4.1.
+  qunitjs: [
+    '1.11.0',
+    '1.12.0-pre',
+    '1.12.0',
+    '1.13.0'
+  ],
+  jasmine: [
+    // Jasmine 2.3.0: there is a bug that makes Jasmine exit and kill the process.
+    // https://github.com/jasmine/jasmine-npm/blob/v2.99.0/release_notes/2.3.1.md
+    // Fixed in jasmine@2.3.1.
+    '2.3.0',
+    // Jasmine 2.5.0: the same bug as in the 2.3.0 version.
+    // https://github.com/jasmine/jasmine-npm/issues/88
+    // Re-fixed in jasmine@2.5.1.
+    '2.5.0'
+  ],
+  mocha: [
+    '0.3.0',
+    '0.3.1',
+    '0.3.2',
+    '0.3.3',
+    '0.3.4',
+    '0.3.6',
+    '0.4.0',
+    '0.5.0',
+    '0.6.0',
+    '0.7.0',
+    '0.7.1',
+    '0.8.0',
+    '0.9.0',
+    '0.10.0',
+    '0.10.1',
+    '0.10.2',
+    '0.11.0',
+    '0.12.0',
+    '0.12.1',
+    '0.13.0',
+    '0.14.0',
+    '0.14.1',
+    '1.0.0',
+    '1.0.1',
+    '1.0.2',
+    '1.0.3',
+    '1.1.0',
+    '1.2.0',
+    '1.2.1',
+    '1.2.2',
+    '1.3.0',
+    '1.3.1',
+    '1.3.2',
+    '1.4.0',
+    '1.4.1',
+    '1.4.2',
+    '1.4.3',
+    '1.5.0',
+    '1.6.0',
+    '1.7.0',
+    '1.7.1',
+    '1.7.2',
+    '1.7.3',
+    '1.7.4',
+    '1.8.0',
+    '1.8.1',
+    '1.8.2',
+    '1.9.0',
+    '1.10.0',
+    '1.11.0',
+    '1.12.0',
+    '1.12.1',
+    '1.13.0',
+    '1.14.0',
+    '1.15.0',
+    '1.15.1',
+    '1.16.0',
+    '1.16.1',
+    '1.16.2',
+    '1.17.0',
+    '1.17.1',
+    '2.1.0',
+    // Mocha 2.5.0: is not working because of a missing dependency, this is Mocha's
+    // fault @see
+    // https://github.com/mochajs/mocha/blob/master/CHANGELOG.md#251--2016-05-23.
+    '2.5.0'
+  ]
 };
--- test/versions/versions-reporting.js
+++ test/versions/versions-reporting.js
@@ -1,29 +1,36 @@
 const childProcess = require('child_process');
+const fs = require('fs');
 const chalk = require('chalk');
-const frameworks = ['QUnitJS', 'Jasmine', 'Mocha'];
+const packages = [
+  'qunitjs',
+  'jasmine',
+  'mocha'
+];
 
 console.log();
+
+const originalLock = fs.readFileSync('package-lock.json', 'utf8');
 
 /**
  * Takes each version of each framework available on npm and runs the tests
  * against it, making in the end a summary of versions which are working with
  * the js-reporters adapters and which don't.
  */
-frameworks.forEach(function (framework) {
-  let command = 'npm view ' + framework.toLowerCase() + ' versions';
+packages.forEach(function (pgk) {
+  let command = 'npm view ' + pgk + ' versions';
   const output = childProcess.execSync(command).toString()
     .replace(/[[]|]|'| |\n/g, '');
   const versions = output.split(',');
   const workingVersions = [];
   const notWorkingVersions = [];
 
-  console.log(chalk.underline.bold.green(framework));
+  console.log(chalk.underline.bold.green(pgk));
   console.log();
 
   versions.forEach(function (version) {
     console.log(chalk.dim('Testing version: ' + version));
 
-    command = 'npm install ' + framework.toLowerCase() + '@' + version;
+    command = 'npm install ' + pgk + '@' + version;
     childProcess.execSync(command, {
       stdio: ['ignore', 'ignore', 'ignore']
     });
@@ -37,7 +44,7 @@ frameworks.forEach(function (framework) {
     }
   });
 
-  module.exports[framework.toLowerCase()] = notWorkingVersions;
+  module.exports[pgk] = notWorkingVersions;
 
   console.log();
   console.log(chalk.green('Working: ' + workingVersions.join(', ') + ';'));
@@ -45,7 +52,8 @@ frameworks.forEach(function (framework) {
   console.log();
 });
 
-// Install the versions from package.json.
-childProcess.execSync('npm install', {
+// Restore package-lock.json and re-install based on that.
+fs.writeFileSync('package-lock.json', originalLock);
+childProcess.execSync('npm ci', {
   stdio: ['ignore', 'ignore', 'ignore']
 });
