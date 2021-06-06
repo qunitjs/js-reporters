83bce595e0a7540cf7b317ab31f7a94c453c5c62
Timo Tijhof <krinklemail@gmail.com>
2021-06-06 19:24:42 +0100

    Reporter: Recognise "Todo" tests from QUnit
    This wasn't previously done because versions that support this feature
    also have a jsreporters-compatible event emitter built-in, but this
    isn't always used (such as in browserstack-runner). So for completion
    support it here as well just in case.
    
    Ref https://github.com/qunitjs/qunit/issues/1622.

--- test/integration/adapters.js
+++ test/integration/adapters.js
@@ -94,118 +94,127 @@ function fixExpectedData (adapter, expectedData) {
   });
 }
 
-QUnit.module('Adapters integration', function () {
-  Object.keys(runAdapters).forEach(function (adapter) {
-    QUnit.module(adapter + ' adapter', hooks => {
-      // Re-require for each adapter because we change the expected data.
-      const expectedData = rerequire('./reference-data.js');
-      fixExpectedData(adapter, expectedData);
+const integrations = [
+  { key: 'main', name: 'Adapters main integration', referenceFile: './reference-data.js' },
 
-      const collectedData = [];
+  // This is only implemented by QUnit currently
+  { key: 'todo', name: 'Adapters todo integration', referenceFile: './reference-data-todo.js' }
+];
 
-      hooks.before(assert => {
-        const done = assert.async();
-        runAdapters[adapter](
-          collectDataFromRunner.bind(null, collectedData, done)
-        );
-      });
+integrations.forEach(function (integration) {
+  QUnit.module(integration.name, function () {
+    Object.keys(runAdapters[integration.key]).forEach(function (adapter) {
+      QUnit.module(adapter + ' adapter', hooks => {
+        // Re-require for each adapter because we mutate the expected data.
+        const expectedData = rerequire(integration.referenceFile);
+        fixExpectedData(adapter, expectedData);
 
-      // Fist check that, overall, all expected events were emitted and in order.
-      test('Emitted events names', assert => {
-        assert.propEqual(
-          collectedData.map(pair => pair[0]),
-          expectedData.map(pair => pair[0]),
-          'Event names'
-        );
-      });
+        const collectedData = [];
 
-      test('Event "testStart" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'testStart');
-        const expecteds = expectedData.filter(pair => pair[0] === 'testStart');
-        assert.propEqual(
-          actuals.map(expected => expected[1].name),
-          expecteds.map(pair => pair[1].name),
-          'Test names'
-        );
-        expecteds.forEach((expected, i) => {
-          assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for testStart#${i}`
+        hooks.before(assert => {
+          const done = assert.async();
+          runAdapters[integration.key][adapter](
+            collectDataFromRunner.bind(null, collectedData, done)
           );
         });
-      });
 
-      test('Event "testEnd" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'testEnd');
-        const expecteds = expectedData.filter(pair => pair[0] === 'testEnd');
-        assert.propEqual(
-          actuals.map(expected => expected[1].name),
-          expecteds.map(pair => pair[1].name),
-          'Test names'
-        );
-        expecteds.forEach((expected, i) => {
+        // Fist check that, overall, all expected events were emitted and in order.
+        test('Emitted events names', assert => {
           assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for testEnd#${i}`
+            collectedData.map(pair => pair[0]),
+            expectedData.map(pair => pair[0]),
+            'Event names'
           );
         });
-      });
 
-      test('Event "suiteStart" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'suiteStart');
-        const expecteds = expectedData.filter(pair => pair[0] === 'suiteStart');
-        assert.propEqual(
-          actuals.map(expected => expected[1].name),
-          expecteds.map(pair => pair[1].name),
-          'Suite names'
-        );
-        expecteds.forEach((expected, i) => {
+        test('Event "testStart" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'testStart');
+          const expecteds = expectedData.filter(pair => pair[0] === 'testStart');
           assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for suiteStart#${i}`
+            actuals.map(expected => expected[1].name),
+            expecteds.map(pair => pair[1].name),
+            'Test names'
           );
+          expecteds.forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for testStart#${i}`
+            );
+          });
         });
-      });
 
-      test('Event "suiteEnd" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'suiteEnd');
-        const expecteds = expectedData.filter(pair => pair[0] === 'suiteEnd');
-        assert.propEqual(
-          actuals.map(expected => expected[1].name),
-          expecteds.map(pair => pair[1].name),
-          'Suite names'
-        );
-        expecteds.forEach((expected, i) => {
+        test('Event "testEnd" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'testEnd');
+          const expecteds = expectedData.filter(pair => pair[0] === 'testEnd');
           assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for suiteEnd#${i}`
+            actuals.map(expected => expected[1].name),
+            expecteds.map(pair => pair[1].name),
+            'Test names'
           );
+          expecteds.forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for testEnd#${i}`
+            );
+          });
         });
-      });
 
-      test('Event "runStart" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'runStart');
-        expectedData.filter(pair => pair[0] === 'runStart').forEach((expected, i) => {
+        test('Event "suiteStart" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'suiteStart');
+          const expecteds = expectedData.filter(pair => pair[0] === 'suiteStart');
           assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for runStart#${i}`
+            actuals.map(expected => expected[1].name),
+            expecteds.map(pair => pair[1].name),
+            'Suite names'
           );
+          expecteds.forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for suiteStart#${i}`
+            );
+          });
         });
-      });
 
-      test('Event "runEnd" data', assert => {
-        const actuals = collectedData.filter(pair => pair[0] === 'runEnd');
-        expectedData.filter(pair => pair[0] === 'runEnd').forEach((expected, i) => {
+        test('Event "suiteEnd" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'suiteEnd');
+          const expecteds = expectedData.filter(pair => pair[0] === 'suiteEnd');
           assert.propEqual(
-            actuals[i][1],
-            expected[1],
-            `Event data for runEnd#${i}`
+            actuals.map(expected => expected[1].name),
+            expecteds.map(pair => pair[1].name),
+            'Suite names'
           );
+          expecteds.forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for suiteEnd#${i}`
+            );
+          });
+        });
+
+        test('Event "runStart" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'runStart');
+          expectedData.filter(pair => pair[0] === 'runStart').forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for runStart#${i}`
+            );
+          });
+        });
+
+        test('Event "runEnd" data', assert => {
+          const actuals = collectedData.filter(pair => pair[0] === 'runEnd');
+          expectedData.filter(pair => pair[0] === 'runEnd').forEach((expected, i) => {
+            assert.propEqual(
+              actuals[i][1],
+              expected[1],
+              `Event data for runEnd#${i}`
+            );
+          });
         });
       });
     });
